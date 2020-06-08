a3f7240a45bab55221dddf553af4f8a4c262903f
steven <kawooyastevenug@gmail.com>
2020-06-08 15:30:26 +0300

    Cleaned app.js, added formik ui form to new project

--- uetcldash/front/src/App.js
+++ uetcldash/front/src/App.js
@@ -1,287 +1,3 @@
-// import React from 'react';
-// import { useForm } from 'react-hook-form';
-
-// export default function App() {
-//   const { register, handleSubmit, errors } = useForm();
-//   const onSubmit = data => console.log(data);
-//   console.log(errors);
-
-//   return (
-//     <form onSubmit={handleSubmit(onSubmit)}>
-//       <input type="text" placeholder="First name" name="First name" ref={register({required: true, maxLength: 80})} />
-//       <input type="text" placeholder="Last name" name="Last name" ref={register({required: true, maxLength: 100})} />
-//       <input type="text" placeholder="Email" name="Email" ref={register({required: true, pattern: /^\S+@\S+$/i})} />
-//       <input type="tel" placeholder="Mobile number" name="Mobile number" ref={register({required: true, minLength: 6, maxLength: 12})} />
-//       <select name="Title" ref={register({ required: true })}>
-//         <option value="Mr">Mr</option>
-//         <option value="Mrs">Mrs</option>
-//         <option value="Miss">Miss</option>
-//         <option value="Dr">Dr</option>
-//       </select>
-
-//       <input name="Developer" type="radio" value="Yes" ref={register({ required: true })}/>
-//       <input name="Developer" type="radio" value="No" ref={register({ required: true })}/>
-
-//       <input type="submit" />
-//     </form>
-//   );
-// }
-
-// import React, { Component } from "react";
-// import ReactDOM from "react-dom";
-// import { Formik } from "formik";
-// import * as yup from "yup";
-// import {
-//     Form,
-//     Radio,
-//     Input,
-//     Datepicker,
-//     Select,
-//     PhoneInput,
-//     Toggle,
-//     DropZone,
-//     Textarea,
-//     Checkbox,
-//     SubmitBtn,
-//     Button,
-// } from "react-formik-ui";
-
-// const formik = useFormik()
-// const onSubmit = (data) => alert(JSON.stringify(data));
-
-// const getSchema2 = () => {
-//     return yup.object().shape({
-//         Project: yup.string().required("Project name is required"),
-
-//     });
-// };
-// const App = () => {
-//     return (
-//         <Formik
-//             initialValues={{
-//                 project: "",
-//                 rapstart: "",
-//                 rapend: "",
-//                 dpdstart: "",
-//                 dpdend: "",
-//                 resettlestart: "",
-//                 resettleend: "",
-//                 supervisionstart: "",
-//                 supervisionend: "",
-//                 epcstart: "",
-//                 epcend: "",
-//                 performstart: "",
-//                 performend: "",
-//                 advancestart: "",
-//                 advanceend: "",
-//                 insurancestart: "",
-//                 insuranceend: "",
-//                 comment: "",
-
-//             }}
-//             validationSchema={getSchema2}
-//             onSubmit={onSubmit}
-//         >
-//             {({ values }) => (
-//                 <Form mode="themed">
-//                     <fieldset>
-//                         <legend>Project contract details</legend>
-//                         <div style={{ margin: "25px", padding: "0px", color:"blue" }}>
-//                             <Input
-//                                 name="project"
-//                                 label="Project name"
-//                                 hint="This is a hint message"
-//                                 required
-//                             />
-//                         </div>
-//                         <div style={{ margin: "25px", padding: "5px" }}>
-//                         <div style={{ margin: "25px", padding: "5px" }}>RAP</div>
-//                         <Datepicker
-//                             name="rapstart"
-//                             label="Start Date"
-//                             dateFormat="dd.MM.yyyy"
-//                             placeholder="dd.mm.yyyy"
-//                             hint="Please enter start date"
-//                             onChange={formik.handleChange}
-//         value={formik.values.rapstart}
-//                         />
-//                         <Datepicker
-//                             name="rapend"
-//                             label="End Date"
-//                             dateFormat="dd.MM.yyyy"
-//                             placeholder="dd.mm.yyyy"
-//                             hint="Please enter expiry date"
-//                             required
-//                         />
-//                         </div>
-//                         <div style={{ margin: "25px", padding: "5px" }}>
-
-//                         <div style={{ margin: "25px", padding: "5px" }}>PDP Houses Consultant</div>
-//                         <Datepicker
-//                             name="pdpstart"
-//                             label="Start Date"
-//                             dateFormat="dd.MM.yyyy"
-//                             placeholder="dd.mm.yyyy"
-//                             hint="Please enter start date"
-//                             required
-//                         />
-
-//                         <Datepicker
-//                             name="pdpend"
-//                             label="End Date"
-//                             dateFormat="dd.MM.yyyy"
-//                             placeholder="dd.mm.yyyy"
-//                             hint="Please enter expiry date"
-//                             required
-//                         />
-//                         </div>
-
-//                         <div style={{ margin: "25px", padding: "5px" }}>
-//                         <div style={{ margin: "25px", padding: "5px" }}>Resettlement Houses Consultant</div>
-//                         <Datepicker
-//                             name="resettlestart"
-//                             label="Start Date"
-//                             dateFormat="dd.MM.yyyy"
-//                             placeholder="dd.mm.yyyy"
-//                             hint="Please enter start date"
-//                             required
-//                         />
-
-//                         <Datepicker
-//                             name="resettleend"
-//                             label="End Date"
-//                             dateFormat="dd.MM.yyyy"
-//                             placeholder="dd.mm.yyyy"
-//                             hint="Please enter expiry date"
-//                             required
-//                         />
-//                         </div>
-//                         <div style={{ margin: "25px", padding: "5px" }}>
-//                         <div style={{ margin: "25px", padding: "5px" }}>Supervision Consultant</div>
-//                         <Datepicker
-//                             name="supervisionstart"
-//                             label="Start Date"
-//                             dateFormat="dd.MM.yyyy"
-//                             placeholder="dd.mm.yyyy"
-//                             hint="Please enter start date"
-//                             required
-//                         />
-
-//                         <Datepicker
-//                             name="supervisionend"
-//                             label="End Date"
-//                             dateFormat="dd.MM.yyyy"
-//                             placeholder="dd.mm.yyyy"
-//                             hint="Please enter expiry date"
-//                             required
-//                         />
-//                         </div>
-//                         <div style={{ margin: "25px", padding: "5px" }}>
-//                         <div style={{ margin: "25px", padding: "5px" }}>EPC</div>
-//                         <Datepicker
-//                             name="epcstart"
-//                             label="Start Date"
-//                             dateFormat="dd.MM.yyyy"
-//                             placeholder="dd.mm.yyyy"
-//                             hint="Please enter start date"
-//                             required
-//                         />
-
-//                         <Datepicker
-//                             name="epcend"
-//                             label="End Date"
-//                             dateFormat="dd.MM.yyyy"
-//                             placeholder="dd.mm.yyyy"
-//                             hint="Please enter expiry date"
-//                             required
-//                         />
-//                         </div>
-
-//                         <div style={{ margin: "25px", padding: "5px" }}>
-//                         <div style={{ margin: "25px", padding: "5px" }}>Performance Guarantee</div>
-//                         <Datepicker
-//                             name="performstart"
-//                             label="Start Date"
-//                             dateFormat="dd.MM.yyyy"
-//                             placeholder="dd.mm.yyyy"
-//                             hint="Please enter start date"
-//                             required
-//                         />
-
-//                         <Datepicker
-//                             name="performend"
-//                             label="End Date"
-//                             dateFormat="dd.MM.yyyy"
-//                             placeholder="dd.mm.yyyy"
-//                             hint="Please enter expiry date"
-//                             required
-//                         />
-//                         </div>
-
-//                         <div style={{ margin: "25px", padding: "5px" }}>
-//                         <div style={{ margin: "25px", padding: "5px" }}>Advance Payment Guarantee</div>
-//                         <Datepicker
-//                             name="advancestart"
-//                             label="Start Date"
-//                             dateFormat="dd.MM.yyyy"
-//                             placeholder="dd.mm.yyyy"
-//                             hint="Please enter start date"
-//                             required
-//                         />
-//                         <Datepicker
-//                             name="advanceend"
-//                             label="End Date"
-//                             dateFormat="dd.MM.yyyy"
-//                             placeholder="dd.mm.yyyy"
-//                             hint="Please enter expiry date"
-//                             required
-//                         />
-//                         </div>
-
-//                         <div style={{ margin: "25px", padding: "5px" }}>
-//                         <div style={{ margin: "25px", padding: "5px" }}>Insurance</div>
-//                         <Datepicker
-//                             name="isuranestart"
-//                             label="Start Date"
-//                             dateFormat="dd.MM.yyyy"
-//                             placeholder="dd.mm.yyyy"
-//                             hint="Please enter start date"
-//                             required
-//                         />
-
-//                         <Datepicker
-//                             name="insuranceend"
-//                             label="End Date"
-//                             dateFormat="dd.MM.yyyy"
-//                             placeholder="dd.mm.yyyy"
-//                             hint="Please enter expiry date"
-//                             required
-//                         />
-//                         </div>
-
-//                         <div style={{ margin: "25px", padding: "5px" }}>
-//                         <Textarea
-//                                 name="comment"
-//                                 label="Comments"
-//                                 hint="this is optional"
-//                             />
-//                             </div>
-
-// <div>
-//           <button type="submit">Submit</button>
-
-//           <Button onClick={(() => alert('Cancel'))}>Cancel</Button>
-//         </div>
-
-//                     </fieldset>
-//                 </Form>
-//             )}
-//         </Formik>
-//     );
-// };
-
-// export default App;
-
 import "date-fns";
 import React from "react";
 import Button from "@material-ui/core/Button";
@@ -304,14 +20,7 @@ import { Formik, Form } from "formik";
 import * as yup from "yup";
 
 let SignupSchema = yup.object().shape({
-    firstName: yup.string().required("This field is required."),
-    lastName: yup.string().required("This field is required."),
-    email: yup.string().email().required("This field is required."),
-    password: yup
-        .string()
-        .min(6, "Password is too short.")
-        .max(20, "Password is too long.")
-        .required("This field is required."),
+    project: yup.string().required("This field is required."),
 });
 
 const useStyles = makeStyles((theme) => ({
@@ -389,7 +98,7 @@ export const App = () => {
                                         <Box
                                             boxShadow={5}
                                             bgcolor="background.paper"
-                                            style={{ borderRadius:10 }}
+                                            style={{ borderRadius: 10 }}
                                         >
                                             <Card
                                                 className={classes.root}
@@ -454,7 +163,7 @@ export const App = () => {
                                         <Box
                                             boxShadow={5}
                                             bgcolor="background.paper"
-                                            style={{ borderRadius:10 }}
+                                            style={{ borderRadius: 10 }}
                                         >
                                             <Card
                                                 className={classes.root}
@@ -519,7 +228,7 @@ export const App = () => {
                                         <Box
                                             boxShadow={5}
                                             bgcolor="background.paper"
-                                            style={{ borderRadius:10 }}
+                                            style={{ borderRadius: 10 }}
                                         >
                                             <Card
                                                 className={classes.root}
@@ -531,7 +240,8 @@ export const App = () => {
                                                         variant="h5"
                                                         component="h2"
                                                     >
-                                                        Resettlement Houses Contractor 
+                                                        Resettlement Houses
+                                                        Contractor
                                                     </Typography>
                                                     <Grid
                                                         container
@@ -584,7 +294,7 @@ export const App = () => {
                                         <Box
                                             boxShadow={5}
                                             bgcolor="background.paper"
-                                            style={{ borderRadius:10 }}
+                                            style={{ borderRadius: 10 }}
                                         >
                                             <Card
                                                 className={classes.root}
@@ -649,7 +359,7 @@ export const App = () => {
                                         <Box
                                             boxShadow={5}
                                             bgcolor="background.paper"
-                                            style={{ borderRadius:10 }}
+                                            style={{ borderRadius: 10 }}
                                         >
                                             <Card
                                                 className={classes.root}
@@ -714,7 +424,7 @@ export const App = () => {
                                         <Box
                                             boxShadow={5}
                                             bgcolor="background.paper"
-                                            style={{ borderRadius:10 }}
+                                            style={{ borderRadius: 10 }}
                                         >
                                             <Card
                                                 className={classes.root}
@@ -779,7 +489,7 @@ export const App = () => {
                                         <Box
                                             boxShadow={5}
                                             bgcolor="background.paper"
-                                            style={{ borderRadius:10 }}
+                                            style={{ borderRadius: 10 }}
                                         >
                                             <Card
                                                 className={classes.root}
@@ -791,7 +501,8 @@ export const App = () => {
                                                         variant="h5"
                                                         component="h2"
                                                     >
-                                                        Advance Payment Guarantee
+                                                        Advance Payment
+                                                        Guarantee
                                                     </Typography>
                                                     <Grid
                                                         container
@@ -840,11 +551,11 @@ export const App = () => {
                                         </Box>
                                     </Grid>
 
-                                    <Grid container >
+                                    <Grid container>
                                         <Box
                                             boxShadow={5}
                                             bgcolor="background.paper"
-                                            style={{ borderRadius:10 }}
+                                            style={{ borderRadius: 10 }}
                                         >
                                             <Card
                                                 className={classes.root}
