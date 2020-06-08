deeb5137bedf1b4ad22c8265487f37207c52f3cd
steven <kawooyastevenug@gmail.com>
2020-06-08 15:38:43 +0300

    costimetics

--- uetcldash/front/src/App.js
+++ uetcldash/front/src/App.js
@@ -93,6 +93,21 @@ export const App = () => {
                     {({ errors, handleChange, touched }) => (
                         <Form className={classes.form}>
                             <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                            <Grid item xs={12} sm={6}>
+                                        <TextField
+                                            error={
+                                                errors.firstName &&
+                                                touched.firstName
+                                            }
+                                            name="project"
+                                            variant="outlined"
+                                            fullWidth
+                                            onChange={handleChange}
+                                            id="project"
+                                            label="Project"
+                                           
+                                        />
+                                    </Grid>
                                 <Grid container spacing={2}>
                                     <Grid container>
                                         <Box
@@ -616,100 +631,7 @@ export const App = () => {
                                         </Box>
                                     </Grid>
 
-                                    <Grid item xs={12} sm={6}>
-                                        <TextField
-                                            error={
-                                                errors.firstName &&
-                                                touched.firstName
-                                            }
-                                            autoComplete="fname"
-                                            name="firstName"
-                                            variant="outlined"
-                                            fullWidth
-                                            onChange={handleChange}
-                                            id="firstName"
-                                            label="First Name"
-                                            autoFocus
-                                            helperText={
-                                                errors.firstName &&
-                                                touched.firstName
-                                                    ? errors.firstName
-                                                    : null
-                                            }
-                                        />
                                     </Grid>
-                                    <Grid item xs={12} sm={6}>
-                                        <TextField
-                                            error={
-                                                errors.lastName &&
-                                                touched.lastName
-                                            }
-                                            variant="outlined"
-                                            fullWidth
-                                            onChange={handleChange}
-                                            id="lastName"
-                                            label="Last Name"
-                                            name="lastName"
-                                            autoComplete="lname"
-                                            helperText={
-                                                errors.lastName &&
-                                                touched.lastName
-                                                    ? errors.lastName
-                                                    : null
-                                            }
-                                        />
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <TextField
-                                            error={
-                                                errors.email && touched.email
-                                            }
-                                            variant="outlined"
-                                            fullWidth
-                                            onChange={handleChange}
-                                            id="email"
-                                            label="Email Address"
-                                            name="email"
-                                            autoComplete="email"
-                                            helperText={
-                                                errors.email && touched.email
-                                                    ? errors.email
-                                                    : null
-                                            }
-                                        />
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <TextField
-                                            error={
-                                                errors.password &&
-                                                touched.password
-                                            }
-                                            variant="outlined"
-                                            fullWidth
-                                            onChange={handleChange}
-                                            name="password"
-                                            label="Password"
-                                            type="password"
-                                            id="password"
-                                            autoComplete="current-password"
-                                            helperText={
-                                                errors.password &&
-                                                touched.password
-                                                    ? errors.password
-                                                    : null
-                                            }
-                                        />
-                                    </Grid>
-                                </Grid>
-                                <Button
-                                    type="submit"
-                                    fullWidth
-                                    variant="contained"
-                                    color="primary"
-                                    className={classes.submit}
-                                >
-                                    Sign Up
-                                </Button>
                             </MuiPickersUtilsProvider>
                         </Form>
                     )}
