76486c844c2ea13acd8b33d521e5dd619a4e5468
steven <kawooyastevenug@gmail.com>
2020-07-03 20:02:52 +0300

    modified app.js navigation for landnig to be first component

--- uetcldash/front/src/App.js
+++ uetcldash/front/src/App.js
@@ -2,40 +2,44 @@ import React from "react";
 import Clock from "./components/Clock";
 import Counter from "./components/Counter";
 import NewProject from "./components/NewProject";
+import Landing from "./components/Landing";
+import { Provider } from "react-redux";
+import store from "./store";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
 } from "react-router-dom";
 
-
 class App extends React.Component {
-  state = {
-    redirect: true
-  };
-
-  render() {
-    const notfound = () => {
-      const { redirect } = this.state;
-      if (redirect) {
-        return <Redirect to="/newproject" />;
-      }
+    state = {
+        redirect: true,
     };
-    return (
-      <Router>
-        <div className="App">
-          <Switch>
-            <Route path="/" exact component={NewProject} />
-            <Route path="/clock" component={Clock} />
-            <Route path="/counter" component={Counter} />
-            <Route component={notfound} />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+
+    render() {
+        const notfound = () => {
+            const { redirect } = this.state;
+            if (redirect) {
+                return <Redirect to="/newproject" />;
+            }
+        };
+        return (
+            <Provider store={store}>
+                <Router>
+                    <div className="App">
+                        <Switch>
+                            <Route path="/" exact component={Landing} />
+                            <Route path="/clock" component={Clock} />
+                            <Route path="/newproject" component={NewProject} />
+                            <Route path="/counter" component={Counter} />
+                            <Route component={notfound} />
+                        </Switch>
+                    </div>
+                </Router>
+            </Provider>
+        );
+    }
 }
 
 export default App;
-
--- uetcldash/front/src/components/Landing.js
+++ uetcldash/front/src/components/Landing.js
@@ -1,52 +1,45 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { getDates } from "./actions/datesActions";
+import { getDates } from "../actions/datesActions";
 import { Provider } from "react-redux";
-import store from "./store";
-import axios from "axios";
+
+// import axios from "axios";
 
 class Landing extends Component {
-  componentDidMount(){
-    this.props.getDates();
-  }
-  //   axios.get(`http://127.0.0.1:8000/api/dates/`)
-  //     .then(res => {
-  //       const dates = res.data;
-  //       this.setState({ dates });
-  //     })
-  // }
+    componentDidMount() {
+        this.props.getDates();
+    }
+    //   axios.get(`http://127.0.0.1:8000/api/dates/`)
+    //     .then(res => {
+    //       const dates = res.data;
+    //       this.setState({ dates });
+    //     })
+    // }
 
-  render(){
-const date = this.props;
-const datesItem = this.props.dates.map(date =>(
-  <div key={date.id}>
-    <div>
-      Project: {date.project}
-    </div>
-    <div>
-      Rap
-      start date: {date.rapstart}
-      end date: {date.rapend}
-    </div>
-  </div>
- 
-));
+    render() {
+        const date = this.props;
+        const datesItem = this.props.dates.map((date) => (
+            <div key={date.id}>
+                <div>Project: {date.project}</div>
+                <div>
+                    Rap start date: {date.rapstart}
+                    end date: {date.rapend}
+                </div>
+            </div>
+        ));
 
- return(
-<Provider store = {store}>
-   <div>
-    <div> Project contracts</div>
-    <div> {datesItem} </div>
-
-   </div>
-   </Provider>
- );
-}
+        return (
+            <div>
+                <div> Project contracts</div>
+                <div> {datesItem} </div>
+            </div>
+        );
+    }
 }
 
-const mapStateToProps = state => ({
-  dates: state.dates.items
+const mapStateToProps = (state) => ({
+    dates: state.dates.items,
 });
 
-export default connect(mapStateToProps, {getDates}) (Landing);
+export default connect(mapStateToProps, { getDates })(Landing);
