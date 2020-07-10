b986ad4bbe507af94ccce94cbcb8eb6377347381
Irenyak1 <inyakate@gmail.com>
2020-07-10 19:21:42 +0300

    Add after add css to the big file

--- uetcldash/front/src/components/DisplayData.js
+++ uetcldash/front/src/components/DisplayData.js
@@ -31,90 +31,120 @@ class DisplayData extends React.Component {
 
         {items.map(item => (
           <div key={item.id}>
-            <div id="project">
-              <h2>{item.project}</h2>
-            </div>
-            <div className="projects">
-              <div className="project">
-                <div className="details">
-                  <div id="contracts">
-                    <h4>RAP Consultant</h4>
-                    <div>
-                      <p>Start Date: {item.rapstart}</p>
-                      <p>End date: {item.rapend}</p>
-                      <p>Time Left</p>
+            <div className="overallcontainer">
+              <div id="project">
+                <h2>Project Name: {item.project}</h2>
+              </div>
+              <div className="projects">
+                <div className="project">
+                  <div className="details">
+                    <div id="contracts">
+                      <h4>RAP Consultant</h4>
+                      <div>
+                        <p>Start Date: {item.rapstart}</p>
+                        <p>End date: {item.rapend}</p>
+                        <p>Time Left</p>
+                      </div>
                     </div>
-                  </div>
-
-                  <div id="contracts">
-                    <h4>PDP Houses Consultant</h4>
-                    <div>
-                      <p>Start Date: {item.pdpstart}</p>
-                      <p>End date: {item.pdpend}</p>
-                      <p>Time Left</p>
-                    </div>
-                  </div>
-
-                  <div id="contracts">
-                    <h4>Resettlement Houses Consultant</h4>
-                    <div>
-                      <p>Start Date: {item.resettlestart}</p>
-                      <p>End date: {item.resettleend}</p>
-                      <p>Time Left</p>
-                    </div>
-                  </div>
-
-                  <div id="contracts">
-                    <h4>Supervision Consultant</h4>
-                    <div>
-                      <p>Start Date: {item.supervisionstart}</p>
-                      <p>End date: {item.supervisionend}</p>
-                      <p>Time Left</p>
-                    </div>
-                  </div>
-
-                  <div id="contracts">
-                    <h4>EPC</h4>
-                    <div>
-                      <p>Start Date: {item.epcstart}</p>
-                      <p>End date: {item.epcend}</p>
-                      <p>Time Left</p>
-                    </div>
-                  </div>
-
-                  <div id="contracts">
-                    <h4>Performance Guarantee</h4>
-                    <div>
-                      <p>Start Date: {item.performstart}</p>
-                      <p>End date: {item.performend}</p>
-                      <p>Time Left</p>
-                    </div>
-                  </div>
-
-                  <div id="contracts">
-                    <h4>Advance Payment Guarantee</h4>
-                    <div>
-                      <p>Start Date: {item.advancestart}</p>
-                      <p>End date: {item.advanceend}</p>
-                      <p>Time Left</p>
-                    </div>
-                  </div>
-
-                  <div id="contracts">
-                    <h4>Insurance</h4>
-                    <div>
-                      <p>Start Date: {item.insurestart}</p>
-                      <p>End date: {item.insureend}</p>
-                      <p>Time Left</p>
-                    </div>
-                  </div>
-                  <div>
-                    <p>
-                      <strong>Comment:</strong>
-                      {item.comment}
-                    </p>
                   </div>
                 </div>
+
+                <div className="project">
+                  <div className="details">
+                    <div id="contracts">
+                      <h4>PDP Houses Consultant</h4>
+                      <div>
+                        <p>Start Date: {item.pdpstart}</p>
+                        <p>End date: {item.pdpend}</p>
+                        <p>Time Left</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="project">
+                  <div className="details">
+                    <div id="contracts">
+                      <h4>Resettlement Houses Consultant</h4>
+                      <div>
+                        <p>Start Date: {item.resettlestart}</p>
+                        <p>End date: {item.resettleend}</p>
+                        <p>Time Left</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="project">
+                  <div className="details">
+                    <div id="contracts">
+                      <h4>Supervision Consultant</h4>
+                      <div>
+                        <p>Start Date: {item.supervisionstart}</p>
+                        <p>End date: {item.supervisionend}</p>
+                        <p>Time Left</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="project">
+                  <div className="details">
+                    <div id="contracts">
+                      <h4>EPC</h4>
+                      <div>
+                        <p>Start Date: {item.epcstart}</p>
+                        <p>End date: {item.epcend}</p>
+                        <p>Time Left</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="project">
+                  <div className="details">
+                    <div id="contracts">
+                      <h4>Performance Guarantee</h4>
+                      <div>
+                        <p>Start Date: {item.performstart}</p>
+                        <p>End date: {item.performend}</p>
+                        <p>Time Left</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="project">
+                  <div className="details">
+                    <div id="contracts">
+                      <h4>Advance Payment Guarantee</h4>
+                      <div>
+                        <p>Start Date: {item.advancestart}</p>
+                        <p>End date: {item.advanceend}</p>
+                        <p>Time Left</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="project">
+                  <div className="details">
+                    <div id="contracts">
+                      <h4>Insurance</h4>
+                      <div>
+                        <p>Start Date: {item.insurestart}</p>
+                        <p>End date: {item.insureend}</p>
+                        <p>Time Left</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <p>
+                  <strong>Comment:</strong>
+                  {item.comment}
+                </p>
               </div>
             </div>
           </div>
