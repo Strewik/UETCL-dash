738ffb44d8b85c3752f0717e3a956d07dd294338
steven <kawooyastevenug@gmail.com>
2020-07-12 18:34:25 +0300

    fix minor bug in merge

--- uetcldash/front/src/components/DisplayData.js
+++ uetcldash/front/src/components/DisplayData.js
@@ -4,200 +4,238 @@ import Clock from "./Clock";
 import { Button } from "react-bootstrap";
 
 class DisplayData extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      isLoaded: false,
-      deadline: ""
-    };
-  }
-  componentDidMount() {
-    fetch("http://127.0.0.1:8000/api/dates/")
-      .then(res => res.json())
-      .then(json => {
-        this.setState({
-          items: json,
-          isLoaded: true
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-  render() {
-    const { isLoaded, items } = this.state;
-    const { history } = this.props;
-    if (!isLoaded) return <div>Loading...</div>;
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded: false,
+            deadline: "",
+        };
+    }
+    componentDidMount() {
+        fetch("http://127.0.0.1:8000/api/dates/")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    isLoaded: true,
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+    render() {
+        const { isLoaded, items } = this.state;
+        if (!isLoaded) return <div>Loading...</div>;
+        return (
+            <div className="App">
+                <h1>Projects Running</h1>
 
-    return (
-      <div className="App">
-        <Button
-          // variant="btn btn-success"
-          onClick={() => history.push("/newproject")}
-        >
-          Add Project
-        </Button>
-        <h1>Projects Running</h1>
+                {items.map((item) => (
+                    <div key={item.id}>
+                        <div className="overallcontainer">
+                            <div id="project">
+                                <h2>Project Name: {item.project}</h2>
+                            </div>
+                            <div className="projects">
+                                <div className="project">
+                                    <div className="details">
+                                        <div id="contracts">
+                                            <h4>RAP Consultant</h4>
+                                            <div>
+                                                <p>
+                                                    Start Date: {item.rapstart}
+                                                </p>
+                                                <p>End date: {item.rapend}</p>
+                                                <p>
+                                                    Time Left:
+                                                    <Clock
+                                                        deadline={item.rapend}
+                                                    />
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
 
-        {items.map(item => (
-          <div key={item.id}>
-            <div className="overallcontainer">
-              <div id="project">
-                <h2>Project Name: {item.project}</h2>
-              </div>
-              <div className="projects">
-                <div className="project">
-                  <div className="details">
-                    <div id="contracts">
-                      <h4>RAP Consultant</h4>
-                      <div>
-                        <p>Start Date: {item.rapstart}</p>
-                        <p>End date: {item.rapend}</p>
-                        <div>
-                          Time Left:
-                          <Clock deadline={item.rapend} />
+                                <div className="project">
+                                    <div className="details">
+                                        <div id="contracts">
+                                            <h4>PDP Houses Consultant</h4>
+                                            <div>
+                                                <p>
+                                                    Start Date: {item.pdpstart}
+                                                </p>
+                                                <p>End date: {item.pdpend}</p>
+                                                <p>
+                                                    Time Left:
+                                                    <Clock
+                                                        deadline={item.pdpend}
+                                                    />
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="project">
+                                    <div className="details">
+                                        <div id="contracts">
+                                            <h4>
+                                                Resettlement Houses Consultant
+                                            </h4>
+                                            <div>
+                                                <p>
+                                                    Start Date:{" "}
+                                                    {item.resettlestart}
+                                                </p>
+                                                <p>
+                                                    End date: {item.resettleend}
+                                                </p>
+                                                <p>
+                                                    Time Left:
+                                                    <Clock
+                                                        deadline={
+                                                            item.resettleend
+                                                        }
+                                                    />
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="project">
+                                    <div className="details">
+                                        <div id="contracts">
+                                            <h4>Supervision Consultant</h4>
+                                            <div>
+                                                <p>
+                                                    Start Date:{" "}
+                                                    {item.supervisionstart}
+                                                </p>
+                                                <p>
+                                                    End date:{" "}
+                                                    {item.supervisionend}
+                                                </p>
+                                                <p>
+                                                    Time Left:
+                                                    <Clock
+                                                        deadline={
+                                                            item.supervisionend
+                                                        }
+                                                    />
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="project">
+                                    <div className="details">
+                                        <div id="contracts">
+                                            <h4>EPC</h4>
+                                            <div>
+                                                <p>
+                                                    Start Date: {item.epcstart}
+                                                </p>
+                                                <p>End date: {item.epcend}</p>
+                                                <p>
+                                                    Time Left:
+                                                    <Clock
+                                                        deadline={item.epcend}
+                                                    />
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="project">
+                                    <div className="details">
+                                        <div id="contracts">
+                                            <h4>Performance Guarantee</h4>
+                                            <div>
+                                                <p>
+                                                    Start Date:{" "}
+                                                    {item.performstart}
+                                                </p>
+                                                <p>
+                                                    End date: {item.performend}
+                                                </p>
+                                                <p>
+                                                    Time Left:
+                                                    <Clock
+                                                        deadline={
+                                                            item.performend
+                                                        }
+                                                    />
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="project">
+                                    <div className="details">
+                                        <div id="contracts">
+                                            <h4>Advance Payment Guarantee</h4>
+                                            <div>
+                                                <p>
+                                                    Start Date:{" "}
+                                                    {item.advancestart}
+                                                </p>
+                                                <p>
+                                                    End date: {item.advanceend}
+                                                </p>
+                                                <p>
+                                                    Time Left:
+                                                    <Clock
+                                                        deadline={
+                                                            item.advanceend
+                                                        }
+                                                    />
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="project">
+                                    <div className="details">
+                                        <div id="contracts">
+                                            <h4>Insurance</h4>
+                                            <div>
+                                                <p>
+                                                    Start Date:{" "}
+                                                    {item.insurestart}
+                                                </p>
+                                                <p>
+                                                    End date: {item.insureend}
+                                                </p>
+                                                <p>
+                                                    Time Left:
+                                                    <Clock
+                                                        deadline={
+                                                            item.insureend
+                                                        }
+                                                    />
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <p>
+                                    <strong>Comment:</strong>
+                                    {item.comment}
+                                </p>
+                            </div>
                         </div>
-                      </div>
                     </div>
-                  </div>
-                  <button>Delete</button>
-                  <button>Update</button>
-                </div>
-
-                <div className="project">
-                  <div className="details">
-                    <div id="contracts">
-                      <h4>PDP Houses Consultant</h4>
-                      <div>
-                        <p>Start Date: {item.pdpstart}</p>
-                        <p>End date: {item.pdpend}</p>
-                        <div>
-                          Time Left:
-                          <Clock deadline={item.pdpend} />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <button>Delete</button>
-                  <button>Update</button>
-                </div>
-
-                <div className="project">
-                  <div className="details">
-                    <div id="contracts">
-                      <h4>Resettlement Houses Consultant</h4>
-                      <div>
-                        <p>Start Date: {item.resettlestart}</p>
-                        <p>End date: {item.resettleend}</p>
-                        <div>
-                          Time Left:
-                          <Clock deadline={item.resettleend} />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <button>Delete</button>
-                  <button>Update</button>
-                </div>
-
-                <div className="project">
-                  <div className="details">
-                    <div id="contracts">
-                      <h4>Supervision Consultant</h4>
-                      <div>
-                        <p>Start Date: {item.supervisionstart}</p>
-                        <p>End date: {item.supervisionend}</p>
-                        <div>
-                          Time Left:
-                          <Clock deadline={item.supervisionend} />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <button>Delete</button>
-                  <button>Update</button>
-                </div>
-
-                <div className="project">
-                  <div className="details">
-                    <div id="contracts">
-                      <h4>EPC</h4>
-                      <div>
-                        <p>Start Date: {item.epcstart}</p>
-                        <p>End date: {item.epcend}</p>
-                        <div>
-                          Time Left:
-                          <Clock deadline={item.epcend} />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <button>Delete</button>
-                  <button>Update</button>
-                </div>
-
-                <div className="project">
-                  <div className="details">
-                    <div id="contracts">
-                      <h4>Performance Guarantee</h4>
-                      <div>
-                        <p>Start Date: {item.performstart}</p>
-                        <p>End date: {item.performend}</p>
-                        <div>
-                          Time Left:
-                          <Clock deadline={item.performend} />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <button>Delete</button>
-                  <button>Update</button>
-                </div>
-
-                <div className="project">
-                  <div className="details">
-                    <div id="contracts">
-                      <h4>Advance Payment Guarantee</h4>
-                      <div>
-                        <p>Start Date: {item.advancestart}</p>
-                        <p>End date: {item.advanceend}</p>
-                        <div>
-                          Time Left:
-                          <Clock deadline={item.advanceend} />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <button>Delete</button>
-                  <button>Update</button>
-                </div>
-
-                <div className="project">
-                  <div className="details">
-                    <div id="contracts">
-                      <h4>Insurance</h4>
-                      <div>
-                        <p>Start Date: {item.insurestart}</p>
-                        <p>End date: {item.insureend}</p>
-                        <div>
-                          Time Left:
-                          <Clock deadline={item.insureend} />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <button>Delete</button>
-                  <button>Update</button>
-                </div>
-              </div>
-
-              <div>
-                <p>
-                  <strong>Comment:</strong>
-                  {item.comment}
-                </p>
-              </div>
+                ))}
             </div>
         );
     }
